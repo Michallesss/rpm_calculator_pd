8142aad192d7edc347d1a9da88ad95a9c5bc0fec
student <student@TE-CZE-D-08S12A>
2023-12-19 12:54:50 +0100

    Dodaj pliki projektów.

---
+++ KalkulatorRPM/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+for (; ; )
+{
+    string input = Console.ReadLine();
+    if (input == null) break;
+
+    Stack<string> stack = new Stack<string>(input.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+    if (stack.Count == 0) continue;
+
+    double result = calculate(stack);
+    Console.WriteLine(result);
+}
+
+double calculate(Stack<string> stack)
+{
+    string stack2 = stack.Pop();
+    double a, b;
+
+    if (!Double.TryParse(stack2, out a))
+    {
+        b = calculate(stack);
+        a = calculate(stack);
+        if (stack2 == "+") a += b;
+        else if (stack2 == "-") a -= b;
+        else
+        {
+            a = 0;
+            Console.WriteLine("lipa");
+        }
+    } 
+    return a;   
+}
